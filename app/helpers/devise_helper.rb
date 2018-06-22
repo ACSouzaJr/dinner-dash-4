d37f65db3ec91aca746cf1db177340292c4ebcd9
ACSouzaJr <junioracs2000@hotmail.com>
2018-06-22 15:31:17 -0300

    added validation and errors

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 module DeviseHelper
-  # A simple way to show error messages for the current devise resource. If you need
-  # to customize this method, you can either overwrite it in your application helpers or
-  # copy the views to your application.
-  #
-  # This method is intended to stay simple and it is unlikely that we are going to change
-  # it to add more behavior or options.
   def devise_error_messages!
-    return "" if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    flash_alerts = []
+    error_key = 'errors.messages.not_saved'
+
+    if !flash.empty?
+      flash_alerts.push(flash[:error]) if flash[:error]
+      flash_alerts.push(flash[:alert]) if flash[:alert]
+      flash_alerts.push(flash[:notice]) if flash[:notice]
+      error_key = 'devise.failure.invalid'
+    end
+
+    return "" if resource.errors.empty? && flash_alerts.empty?
+    errors = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
+
+    messages = errors.map { |msg| content_tag(:li, msg) }.join
 
     html = <<-HTML
     <div class="alert alert-danger" id="error_explanation">
