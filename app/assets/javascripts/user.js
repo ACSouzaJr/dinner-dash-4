3226360a96832e485e77cfe0b00f9e54b9f1d746
ACSouzaJr <junioracs2000@hotmail.com>
2018-06-29 18:45:59 -0300

    css profile page

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,7 +1,5 @@
-/* data - toggle="table" data - pagination="true" data - page - size="8" data - show - footer="false" */
-
 //remover os detalhes
-$(function () {
+$(document).on('turbolinks:load', function () {
   $('#table').bootstrapTable({
     pagination: true,
     pageSize: 8,
