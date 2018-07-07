919981c0addaa25b093610754e808775a7cc72ee
DAVI <davipedroza@cjr.org.br>
2018-07-07 18:07:49 -0300

    abc

--- app/models/admin.rb
+++
@@ -1,8 +0,0 @@
-class Admin < ApplicationRecord
-
-	  validates :name, presence: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  resources :categories
+  resources :categoria
   devise_for :users,:controllers => { :registrations =>'users/registrations'}
   root :to =>'home#index'
-  get 'admin', to: 'admin#index'
+  resources :users, except: [:create, :new, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
