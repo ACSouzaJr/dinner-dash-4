67308ffa718daa612b222f71f53c7d0b13af49c6
ACSouzaJr <junioracs2000@hotmail.com>
2018-07-12 15:19:37 -0300

    updated: create and update cart actions

--- app/controllers/order_meals_controller.rb
+++ app/controllers/order_meals_controller.rb
@@ -1,13 +1,19 @@
 class OrderMealsController < ApplicationController
+  before_action :set_item, only: [:create, :update]
   
   def create
     #byebug
-    current_cart << order_meal_params
+    #quando tenta adicionar um item que ja existe aumenta quantidade em 1
+    if @item
+      @item['quantity'] = @item['quantity'].to_i + 1
+    else
+      current_cart << order_meal_params
+      #current_cart.uniq! #nao permited duplicação
+    end
   end
 
   def update
     #byebug
-    @item = current_cart.find { |h| h['meal_id'] == params[:order_meal][:meal_id] }
     @item.update(order_meal_params)
   end
   
@@ -21,5 +27,8 @@ class OrderMealsController < ApplicationController
   def order_meal_params
     params.require(:order_meal).permit(:quantity, :meal_id)
   end
-
+  def set_item
+    @item = current_cart.find { |h| h['meal_id'] == params[:order_meal][:meal_id] }
+  end
+  
 end
